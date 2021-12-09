ad31b2f06e34374f6076e65eb27a538768f507f6
fanani03 <afanfanani202@gmail.com>
2021-12-09 12:17:15 +0700

    fanani add file baru dan folder foto

---
+++ hapus-buku.php
@@ -0,0 +1,23 @@
+<?php
+
+
+require 'functions.php';
+
+$id = $_GET["id"];
+// var_dump($id);die;
+if (hapus($id) > 0) {
+    echo "
+        <script>
+            alert('data berhasil di hapus!');
+            document.location.href = 'table-basic.php';
+        </script>
+        ";
+} else{
+    echo "
+        <script>
+            alert('data gagal dihapus!!!');
+            document.location.href = 'table-basic.php';
+        </script>
+        ";
+}
+?>
